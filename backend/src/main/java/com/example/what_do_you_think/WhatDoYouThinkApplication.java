1497f39da033cead328ce805670670677a4033fb
하헌찬 <127941120+HaHeonchan@users.noreply.github.com>
2025-09-14 22:19:17 +0900

    project init

---
+++ backend/src/main/java/com/example/what_do_you_think/WhatDoYouThinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.what_do_you_think;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatDoYouThinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatDoYouThinkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/what_do_you_think/WhatDoYouThinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.what_do_you_think;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatDoYouThinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
